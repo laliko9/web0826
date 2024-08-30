ba3f16cc2e39377354dda752d6784816623c2748
laliko9 <laliko9@naver.com>
2024-08-30 18:02:06 +0900

    0828

---
+++ cdn/src/Card.js
@@ -0,0 +1,35 @@
+function Card(){
+    return (
+        <li className="card_list">
+            <div className="card_img">
+                
+            <div>
+
+                img
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+        </li>
+        <li>
+            <div>
+                <img src="" alt="" >
+                <div></div>
+                </div>
+            </div>
+
+        </li>
+
+
+
+    );
+
+}
